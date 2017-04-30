5b07d69831bb928860d025f25d910c55ff0d4d89
Bhushit Agarwal <bhushit.agarwal@gmail.com>
2017-04-30 09:15:12 -0500

    Added a detailed and structured format of data to enable use of different APIs in future. Also organize data into well-defined events.

---
+++ src/info_detailed.js
@@ -0,0 +1,171 @@
+module.exports = {
+    "FACTS_EN_US" : {
+		"automotive fluid, antifreeze, motor oil, oil, used oil, transmission fluid": {
+			"facts": [
+				{
+					"fact":"Every gallon of used motor oil that is improperly discarded can contaminate 1 million gallons of drinking water, according to the EPA.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-automotive-fluids/"
+					}
+				}
+			],
+			"tips": [
+				{
+					"tip":"Government jurisdictions classify used motor oil and other auto fluids as hazardous waste and require proper disposal by law. If your motor oil, transmission fluid, brake fluid, or antifreeze has been contaminated with any other fluids most locations will not accept it for recycling.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-automotive-fluids/"
+					}
+				}
+			],
+			"actions": [
+			
+			],
+			"category":"automotive-fluids"			
+		},
+		"aerosol": {
+			"facts": [
+				{
+					"fact":"The U.S. banned the use of chlorofluorocarbons (CFCs), the compounds that most publicly linked aerosol can use to ozone layer damage, in 1978. Still, critics of the cans cite the possible damaging effects of hydrocarbons and compressed gases linked to climate change, and of VOCs’ contribution to smog.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-aerosol-cans/"
+					}
+				},
+				{
+					"fact":"Empty aerosol cans are usually not classified as hazardous waste due solely to the substances they once contained, but rather because their contents are highly pressurized and capable of explosion if heated or otherwise mishandled.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-aerosol-cans/"
+					}
+				}
+			],
+			"tips": [
+				{
+					"tip":"An aerosol can must be empty before recycling. But the meaning of 'empty' can be different to recycler. They should never been punctured by anyone other than a certified recycler. Lastly, do not remove the spray nozzle from an aerosol can, as even when the inside contents have been spent, cans are pressurized and can pose a danger if punctured.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-aerosol-cans/"
+					}
+				}
+			],
+			"actions": [
+				
+			],
+			"category":"can"			
+		},
+		"aluminum can": {
+			"facts": [
+				{
+					"fact":"The energy you save by recycling a single aluminum can will run a TV for 3 hours.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-aluminum-cans/"
+					}
+				},
+				{
+					"fact":"In 1996, aluminum recycling saved enough energy to power the city of Pittsburgh for over 6 years.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-aluminum-cans/"
+					}
+				},
+				{
+					"fact":"The aluminum in one single soda is worth about a cent. Some companies and organizations like 'Cans Count' help schools and charities in need of funding organize recycling drives of aluminum cans. Check with local schools and charities to see if they are already participating in such efforts.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-aluminum-cans/"
+					}
+				}
+			],
+			"tips": [
+				
+			],
+			"actions": [
+				
+			],
+			"category":"can"			
+		},
+		"steel can, tin can, soup can, coffee can": {
+			"pre": "Tin cans also called as steel cans are usally used for soup cans, vegetable cans, etc.",
+			"facts": [
+				{
+					"fact":"They make up about 90 percent of the U.S. food can market. About 71 percent of steel cans are recycled, making them one of the most recycled packaging products in America.",
+					"source": {
+						"name":"",
+						"url":""
+					}
+				},
+				{
+					"fact":"Recycling steel saves at least 75 percent of the energy it would take to create steel from raw materials. That\'s enough energy to power 18 million homes.",
+					"source": {
+						"name":"",
+						"url":""
+					}
+				}
+			],
+			"tips": [
+				
+			],
+			"actions": [
+				
+			],
+			"category":"can"			
+		},
+		"book, magazine": {
+			"facts": [
+				{
+					"fact":"Some consumers think glossy paper can\'t be recycled. That may have been true in the early days of recycling, but no longer. About 45 percent of magazines are being recycled today.",
+					"source": {
+						"name":"",
+						"url":""
+					}
+				},
+				{
+					"fact":"Recycled magazines are used to make newspaper, tissues, writing paper and paperboard. Recycling just one ton of paper saves enough energy to power the average American home for six months.",
+					"source": {
+						"name":"",
+						"url":""
+					}
+				}
+			],
+			"tips": [
+				
+			],
+			"actions": [
+				{
+					"action":"If you think your books are in decent condition you should consider reselling your book or donating it to a charitable organization. There are many charities that will take your used books, especially textbooks, and give them to those who cannot afford them.",
+					"source": {
+						"name":"Earth911",
+						"url":"http://earth911.com/recycling-guide/how-to-recycle-books-magazines/"
+					},
+					"type":"find-recycler"
+				}
+			],
+			"category":"paper"			
+		},
+		"paper, newspaper": {
+			"pre": {
+				"paper":"There are several type of paper. Lets take example of newspaper."
+			},
+			"facts": [
+				{
+					"fact":"Recycled newspapers can be made into cereal boxes, egg cartons, pencil barrels, grocery bags, tissue paper and many other products, including new newspapers.",
+					"source": {
+						"name":"",
+						"url":""
+					}
+				}
+			],
+			"tips": [
+				
+			],
+			"actions": [
+				
+			],
+			"category":"paper"			
+		}
+		
+	}
+};
