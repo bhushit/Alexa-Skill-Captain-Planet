0bd6f773cc25e04dd15577b8060f18195383b2ae
Bhushit Agarwal <bhushit.agarwal@gmail.com>
2017-05-08 12:02:45 -0500

    Added Intents to handle fact and tips directly. Changed intent name for RecycleIntent to Recycle. Added boilerplate code to get user's address for future functionality

--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ const info = require('./info');
 const info_detailed = require('./info_detailed');
 
 const APP_ID = process.env.ALEXA_APP_ID;
-var VoiceLabs = require("voicelabs")(process.env.VOICE_LABS_API_KEY);
+const VoiceLabs = require("voicelabs")(process.env.VOICE_LABS_API_KEY);
 
 const handlers = {
     'LaunchRequest': function () {
         this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
-        // If the user either does not reply to the welcome message or says something that is not
-        // understood, they will be prompted again with this text.
+        
         this.attributes.repromptSpeech = this.t('WELCOME_REPROMT');
+		//this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
         VoiceLabs.track(this.event.session, this.event.request.intent.name, this.event.request.intent.slots, this.attributes.speechOutput, (error, response) => {
 			this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
 		});
     },
+	'SessionStartedRequest': function() {
+		this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
+       
+	    this.attributes.repromptSpeech = this.t('WELCOME_REPROMT');
+        VoiceLabs.track(this.event.session, this.event.request.intent.name, this.event.request.intent.slots, this.attributes.speechOutput, (error, response) => {
+			this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+		});
+	},
     'RecycleIntent': function () {
         const itemSlot = this.event.request.intent.slots.item;
         let itemName;
@@ -24,13 +32,89 @@ const handlers = {
             itemName = itemSlot.value.toLowerCase();
 			this.attributes.itemName = itemName;
         }
-
+		
         const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
         const recyclingFacts = this.t('INFO_DETAILED');
 
         if (factExists(recyclingFacts, itemName)) {
 			this.attributes.currentState = 'FACT';
 			const fact = getRandomFact(recyclingFacts, itemName);
+			const feedback = nextSuggestion(this, this.attributes.currentState, recyclingFacts, itemName);
+            this.attributes.speechOutput = fact + '... '+ feedback;
+            this.attributes.repromptSpeech = this.t('REPEAT_MESSAGE');
+			VoiceLabs.track(this.event.session, this.event.request.intent.name, this.event.request.intent.slots, this.attributes.speechOutput, (error, response) => {
+				this.emit(':askWithCard', this.attributes.speechOutput, this.attributes.repromptSpeech, cardTitle, fact);
+			});
+        } else {
+            let speechOutput = this.t('NOT_FOUND_MESSAGE');
+            const repromptSpeech = this.t('NOT_FOUND_REPROMPT');
+            if (itemName) {
+                speechOutput += this.t('NOT_FOUND_WITH_ITEM_NAME', itemName);
+            } else {
+                speechOutput += this.t('NOT_FOUND_WITHOUT_ITEM_NAME');
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes.speechOutput = speechOutput;
+            this.attributes.repromptSpeech = repromptSpeech;
+			
+			VoiceLabs.track(this.event.session, this.event.request.intent.name, this.event.request.intent.slots, speechOutput, (error, response) => {
+				this.emit(':ask', speechOutput, this.attributes.repromptSpeech);
+			});
+        }
+    },
+	'Fact': function () {
+        const itemSlot = this.event.request.intent.slots.item;
+        let itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+			this.attributes.itemName = itemName;
+        }
+		
+        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+        const recyclingFacts = this.t('INFO_DETAILED');
+
+        if (factExists(recyclingFacts, itemName)) {
+			this.attributes.currentState = 'FACT';
+			const fact = getRandomFact(recyclingFacts, itemName);
+			const feedback = nextSuggestion(this, this.attributes.currentState, recyclingFacts, itemName);
+            this.attributes.speechOutput = fact + '... '+ feedback;
+            this.attributes.repromptSpeech = this.t('REPEAT_MESSAGE');
+			VoiceLabs.track(this.event.session, this.event.request.intent.name, this.event.request.intent.slots, this.attributes.speechOutput, (error, response) => {
+				this.emit(':askWithCard', this.attributes.speechOutput, this.attributes.repromptSpeech, cardTitle, fact);
+			});
+        } else {
+            let speechOutput = this.t('NOT_FOUND_MESSAGE');
+            const repromptSpeech = this.t('NOT_FOUND_REPROMPT');
+            if (itemName) {
+                speechOutput += this.t('NOT_FOUND_WITH_ITEM_NAME', itemName);
+            } else {
+                speechOutput += this.t('NOT_FOUND_WITHOUT_ITEM_NAME');
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes.speechOutput = speechOutput;
+            this.attributes.repromptSpeech = repromptSpeech;
+			
+			VoiceLabs.track(this.event.session, this.event.request.intent.name, this.event.request.intent.slots, speechOutput, (error, response) => {
+				this.emit(':ask', speechOutput, this.attributes.repromptSpeech);
+			});
+        }
+    },
+	'Tip': function () {
+        const itemSlot = this.event.request.intent.slots.item;
+        let itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+			this.attributes.itemName = itemName;
+        }
+		
+        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+        const recyclingFacts = this.t('INFO_DETAILED');
+
+        if (tipsExists(recyclingFacts, itemName)) {
+			this.attributes.currentState = 'TIP';
+			const fact = getRandomTip(recyclingFacts, itemName);
 			const feedback = nextSuggestion(this, this.attributes.currentState, recyclingFacts, itemName);
             this.attributes.speechOutput = fact + '... '+ feedback;
             this.attributes.repromptSpeech = this.t('REPEAT_MESSAGE');
@@ -123,6 +207,10 @@ const languageStrings = {
 			SUGGEST_TIP_WITH_ITEM: "Would you like me to suggest some tips for recycling %s?",
 			SUGGEST_ACTION_WITH_ITEM: "Would you like me to suggest some ways to recycle %s?",
 			DEFAULT_REPROMPT: 'What else can I help with?',
+			NOTIFY_MISSING_PERMISSIONS :'Please enable Location permissions in the Amazon Alexa app.',
+			NO_ADDRESS : "It looks like you don't have an address set. You can set your address from the companion app.",
+			LOCATION_FAILURE: "There was an error with the Device Address API. Please try again.",
+			ERROR : "Uh Oh. Looks like something went wrong.",
         },
     },
 };
@@ -214,3 +302,67 @@ function nextSuggestion(context, currentState, dict, itemName)  {
 	}
 	return context.t('DEFAULT_REPROMPT');
 }
+
+/*function performAction(context, item, actionType) {
+	switch(actionType) {
+		case 'find-recycler':
+			const address = getAddress(context);
+			const findNearby = new FindNearby(item, address);
+			var nearbyRecyclers = findNearby.search(); // TODO Clean search results to conversation 
+			break;
+	}
+}*/
+
+/* function getAddress = function(h) {
+        console.info("Starting getAddressHandler()");
+        console.log(h.event);
+        const consentToken = h.event.context.System.user.permissions.consentToken;
+
+        // If we have not been provided with a consent token, this means that the user has not
+        // authorized your skill to access this information. In this case, you should prompt them
+        // that you don't have permissions to retrieve their address.
+        if(!consentToken) {
+            h.emit(":tellWithPermissionCard", h.t('NOTIFY_MISSING_PERMISSIONS'), PERMISSIONS);
+
+            // Lets terminate early since we can't do anything else.
+            console.log("User did not give us permissions to access their address.");
+            console.info("Ending getAddressHandler()");
+            return;
+        }
+
+        const deviceId = h.event.context.System.device.deviceId;
+        const apiEndpoint = h.event.context.System.apiEndpoint;
+
+        const alexaDeviceAddressClient = new AlexaDeviceAddressClient(apiEndpoint, deviceId, consentToken);
+        let deviceAddressRequest = alexaDeviceAddressClient.getFullAddress();
+
+        deviceAddressRequest.then((addressResponse) => {
+            switch(addressResponse.statusCode) {
+                case 200:
+                    console.log("Address successfully retrieved, now responding to user.");
+                    const address = addressResponse.address;
+
+					return address;
+                    break;
+                case 204:
+                    // This likely means that the user didn't have their address set via the companion app.
+                    console.log("Successfully requested from the device address API, but no address was returned.");
+                    h.emit(":tell", h.t('NO_ADDRESS'));
+                    break;
+                case 403:
+                    console.log("The consent token we had wasn't authorized to access the user's address.");
+                    h.emit(":tellWithPermissionCard", h.t('NOTIFY_MISSING_PERMISSIONS'), PERMISSIONS);
+                    break;
+                default:
+                    h.emit(":ask", h.t('LOCATION_FAILURE'), h.t('LOCATION_FAILURE'));
+            }
+
+            console.info("Ending getAddressHandler()");
+        });
+
+        deviceAddressRequest.catch((error) => {
+            h.emit(":tell", h.t('ERROR'));
+            console.error(error);
+            console.info("Ending getAddressHandler()");
+        });
+	}; */
